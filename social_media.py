7e0258cca7bba449b2f82ea386b7255698e1e950
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-06 11:36:01 +0530

    create post with 0 likes and empty comment section

--- social_media.py
+++ social_media.py
@@ -1,4 +1,15 @@
 def initializePlatform():
     Media=[]
-    return f"Current Posts on the media: {Media}"
+    return Media
 # print(initializePlatform())
+
+def createPost(platform,content):
+    post={}
+    post["content"]=content
+    post["likes"]=0
+    post["comments"]=[]
+    platform.append(post)
+    return platform
+# plat=initializePlatform()
+# content="currently working on cspp"
+# print(createPost(plat,content))
