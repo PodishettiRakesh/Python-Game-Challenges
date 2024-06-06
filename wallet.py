1bf053e7a94b305d389b6ae4faa3b330d57f00c4
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-06 10:29:11 +0530

    display balance function is implemented

--- wallet.py
+++ wallet.py
@@ -2,4 +2,11 @@ def initializeWallet():
     wallet={"balance":0,"Trans_History":[]}
     return wallet
 # print(initializeWallet())
+
+def display_balance(wallet):
+    bal=wallet["balance"]
+    print(f"Current Balance in the wallet is: {bal}")
+    return 
+# wallet=initializeWallet()
+# display_balance(wallet)
     
