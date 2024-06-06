a276630caf448e9669dc7ee726670e4f64c22caa
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-06 16:05:27 +0530

    social media main fun is implemented with proper helper functions calling

--- social_media.py
+++ social_media.py
@@ -10,46 +10,26 @@ def createPost(platform,content):
     post["comments"]=[]
     platform.append(post)
     return platform
-# plat=initializePlatform()
-# content="currently working on cspp"
-# print(createPost(plat,content))
+
 
 def viewTimeline(platform):
     for each in platform:
         print(each)
-# platform=initializePlatform()
-# content="currently working on cspp"
-# platform=createPost(platform,content)
-# content="solving given challenges"
-# platform=createPost(platform,content)
-# viewTimeline(platform)
+
 
 def likePost(platform,PostIndex):
-    if PostIndex >= len(platform):
+    if PostIndex >= len(platform) or PostIndex<0:
         return "Invalid post index"
-    post=platform[PostIndex]
-    post["likes"]+=1
+    platform[PostIndex["likes"]]+=1
     return platform
-# platform=initializePlatform()
-# content="currently working on cspp"
-# platform=createPost(platform,content)
-# content="solving given challenges"
-# platform=createPost(platform,content)
-# print(likePost(platform,1))
+
 
 def comment_Post(platform,PostIndex,comment):
-    if PostIndex >= len(platform):
+    if PostIndex >= len(platform) or PostIndex<0:
         return "Invalid post index"
-    post=platform[PostIndex]
-    post["comments"].append(comment)
+    platform[PostIndex]["comments"].append(comment)
     return platform
-# platform=initializePlatform()
-# content="currently working on cspp"
-# platform=createPost(platform,content)
-# content="solving given challenges"
-# platform=createPost(platform,content)
-# platform=likePost(platform,1)
-# print(comment_Post(platform,1,"That's Great"))
+
 
 def startPlatform():
     print("welcome to the social media platform")
