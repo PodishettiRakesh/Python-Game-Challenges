0d19129b26aec73ca296e2cf505f738501c8cc44
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-07 10:02:15 +0530

    parseQuestions fun to create list of dictionaries

--- quiz.py
+++ quiz.py
@@ -3,3 +3,21 @@ def loadData():
     data=f.read()
     return data
 # print(loadData())
+
+def parseQuestions(data):
+    questions=data.split("\n")
+    all_Questions=[]
+    for question in questions:
+        question=question.split(":")
+        print(question)
+        dic={}
+        dic["question_text"]=question[0]
+        dic["choices"]=question[1].split(",")
+        dic["correct_option"]=question[2]
+        dic["max_marks"]=question[3]
+        dic["penality"]=question[4]
+        all_Questions.append(dic)
+    return all_Questions
+# data=loadData()
+# print(parseQuestions(data))
+
