e498b1aeca8f4702329c7e4ad2a4ec598c77f2e7
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-08 21:57:00 +0530

    userChoice fun impleted with proper error handling

---
+++ rock_paper_scisscor.py
@@ -0,0 +1,15 @@
+def getUserChoice():
+    while True:
+        try:
+            userChoice=input("enter your choice: ")
+            userChoice=userChoice.lower()
+            print(userChoice)
+            if userChoice in ["rock","paper","scissor"]:
+                break
+            else:
+                print("please enter valids option")
+        except ValueError:
+            print("please enter valid choice")
+
+    return userChoice
+# print(getUserChoice())
