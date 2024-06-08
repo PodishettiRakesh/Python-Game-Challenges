bdd2920c13bccee41199369ce0eafc61aaaafe41
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-08 23:04:22 +0530

    check pattern consitions is added to become game more complex

--- rock_paper_scisscor.py
+++ rock_paper_scisscor.py
@@ -21,37 +21,66 @@ def getComputerChoice():
     return choice
 # print(getComputerChoice())
 
+user_score=0
+computer_score=0
+
 def determineWinner(user,computer):
     if user==computer:
         return "It's Tie"
     elif user=="rock":
         if computer=="paper":
+            computer_score=computer_score+1
             return "computer won"
         else:
+            user_score=user_score+1
             return "you won"
+        
     
     elif user=="scissor":
         if computer=="rock":
+            computer_score=computer_score+1
             return "computer won"
         else:
+            user_score=user_score+1
             return "you won"
 
     elif user=="paper":
         if computer=="scissor":
+            computer_score=computer_score+1
             return "computer won"
         else:
+            user_score=user_score+1
             return "you won"
 # user=getUserChoice()
 # comp=getComputerChoice()
 # print(determineWinner(user,comp))
 
+def check_pattern(user_choices):
+
 def play():
     print("welcome to the game")
+    print("You can start the game by selecting 'rock', 'scissors' or 'paper'.")
+    print("Choose continuously until you create a pattern.")
+    print("Examples of patterns:")
+    print("Rock, rock, rock, rock, etc.")
+    print("Rock, scissors, rock, scissors, etc.")
+    print("Rock, scissors, paper, rock, scissors, paper, etc.")
+    print("Paper, paper, paper, paper, etc.")
+    print("See that you will lose.")
+    userPattern=[]
     
     while True:
+
         user=getUserChoice()
         comp=getComputerChoice()
+        userPattern.append(user)
+        print(f"you choose: {user}")
+        print(f"computer choice: {comp}")
         print(determineWinner(user,comp))
+
+        if check_pattern(user):
+            print("You formed a pattern. You lose!")
+            break
 
         play=input("y for play again: ")
         if play!="y":
