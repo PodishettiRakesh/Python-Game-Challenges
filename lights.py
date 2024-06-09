e15ff8be847f1771683f40886207a271c18a1cc8
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-09 17:19:29 +0530

    toggle_light fun to toggel the adjacent lights of selected light

--- lights.py
+++ lights.py
@@ -35,3 +35,21 @@ def get_user():
                 print("Invalid row position")
         except ValueError:
             print("enter valid light positions")
+
+def toggle_light(grid,row,col):
+    for i in range(max(0,row-1),min(5,row+2)):
+        for j in range(max(0,col-1),min(5,col+2)):
+            if grid[i][j]==1:
+                grid[i][j]=0
+            else:
+                grid[i][j]=1
+    return grid
+# grid=initialize_grid()
+# print(grid)
+# print_grid(grid)
+# row=2
+# col=3
+# grid=toggle_light(grid,row,col)
+# print("===========")
+# print_grid(grid)
+
