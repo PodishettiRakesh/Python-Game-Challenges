777e288d4b96194e856294f8be52d6d4e55d4826
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-10 14:23:25 +0530

    deck of cards created as dictionary

---
+++ blackJack.py
@@ -0,0 +1,15 @@
+import random
+deck={"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13,"A":14}
+def deck_creation(deck):
+    cards=[]
+    for key,value in deck.items():
+        cards.append(value)
+    random.shuffle(cards)
+    return cards    
+# print(deck_creation(deck))
+
+def dealing_cards(cards):
+    return cards.pop()
+# cards=deck_creation(deck)
+# print(dealing_cards(cards))
+
