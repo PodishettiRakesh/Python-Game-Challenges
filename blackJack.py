2f02809d212cb3679c43cba7031168969c418830
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-10 15:03:59 +0530

    while loop for player1 is implemented

--- blackJack.py
+++ blackJack.py
@@ -1,5 +1,5 @@
 import random
-deck={"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13,"A":14}
+deck={"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10,"A":11}
 def deck_creation(deck):
     cards=[]
     for key,value in deck.items():
@@ -16,5 +16,31 @@ def Calculate_Score(Player_cards):
     score=0
     for card in Player_cards:
         score+=deck[card]
+    if "A" in Player_cards and score>=21:
+        score-=10
     return score
-# print(Calculate_Score(["J","5"]))
+
+# print(Calculate_Score(["5","A"]))
+
+def main():
+    cards=deck_creation(deck)
+    player1=[cards.pop(),cards.pop()]
+    player2=[cards.pop(),cards.pop()]
+    while True:
+        # print(player1)
+        player1_Score=Calculate_Score(player1)
+        print(f"your cards: {player1} and score is: {player1_Score}")
+
+        if player1_Score>=21:
+            print("you loss the game!")
+            break
+        choice=input("enter Y to hit card or N to stand: ")
+        choice=choice.lower()
+        if choice=="y":
+            player1.append(dealing_cards(cards))
+        else:
+            break
+        
+
+    
+main()
