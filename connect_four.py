f2bd6a483485bbd9afd51ddc2f118b12115533e7
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-11 19:46:47 +0530

    function to drop disc in choosen column

--- connect_four.py
+++ connect_four.py
@@ -20,10 +20,35 @@ def get_user_input():
     while True:
         try:
             col_num=int(input("enter your col number to drop the disc: "))
-            if col_num>7 or col_num<1:
+            if col_num>col-1 or col_num<0:
                 print("col position out of grid")
             else:
                 return col_num
         except ValueError:
             print("Error in selecting column to drop disc! try again")
 # print(get_user_input())
+row=6
+col=7
+def drop_disc(grid,col_num,disc):
+    i=0
+    while i<row:
+        if grid[i][col_num]=="_" and i!=5:
+            if grid[i+1][col_num]!="_":
+                grid[i][col_num]=disc
+                break
+            i+=1
+        else:
+            grid[i][col_num]=disc
+            break
+    return grid
+# grid=generate_grid(row,col)
+# display_grid(grid)
+# print("---------------")
+# grid1=drop_disc(grid,1,"x")
+# display_grid(grid1)
+# grid2=drop_disc(grid1,1,"x")
+# print("---------------")
+# display_grid(grid2)
+# grid3=drop_disc(grid2,4,"F")
+# print("---------------")
+# display_grid(grid3)
