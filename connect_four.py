1a8256e725ee00683843239600c23df2ed07528b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-11 23:35:39 +0530

    main function implemeted which ties all together

--- connect_four.py
+++ connect_four.py
@@ -52,3 +52,69 @@ def drop_disc(grid,col_num,disc):
 # grid3=drop_disc(grid2,4,"F")
 # print("---------------")
 # display_grid(grid3)
+def right_connect(grid,i,j,disc):
+    # print("-------------------cdcj")
+    # print(i,j)
+    if j+1<len(grid[0]) and j+2<len(grid[0]) and j+3<len(grid[0]):
+        if grid[i][j]==disc and grid[i][j+1]==disc  and grid[i][j+2]==disc and grid[i][j+3]==disc :
+            print(grid[i][j],grid[i][j+1],grid[i][j+2],grid[i][j+3])
+            print("yes")
+            return True
+        else:
+            return False
+    else:
+        return False
+def vertical_connect(grid,disc):
+    for i in range(len(grid)-3):
+        for j in range(len(grid[i])):
+            if grid[i][j]==disc and grid[i+1][j]==disc and grid[i+2][j]==disc and grid[i+3][j]==disc:
+                return True
+    return False
+
+def diagonal_connect(grid,i,j,disc):
+    if i+3<len(grid) and j-3>=0:
+        if grid[i][j]==disc and grid[i+1][j-1]==disc and grid[i+2][j-2]==disc and grid[i+3][j-3]==disc:
+            return True
+    if i+3<len(grid) and j+3<len(grid[0]):
+        if grid[i][j]==disc and grid[i+1][j+1]==disc and grid[i+2][j+2]==disc and grid[i+3][j+3]==disc:
+            return True
+        
+    return False
+    
+
+
+def checkwin(grid,disc):
+    for i in range(len(grid)):
+        # print(len(grid),"len grid")
+        for j in range(len(grid[i])):
+            # print(len(grid[i]),"grid[i]")
+            if right_connect(grid,i,j,disc):
+                # print(i,j,"i,,j")
+                return True
+            elif diagonal_connect(grid,i,j,disc):
+                return True
+    if vertical_connect(grid,disc):
+        return True  
+    
+    return False   
+            
+def main():
+    print('welcome to the connect four game')
+    grid=generate_grid(6,7)
+    current_player=0
+    desk=["x","y"]
+    
+    while True:
+        print("connect your desk as straight four in any direction to win the game")
+        display_grid(grid)
+        print(f"player{current_player+1}'s turn: ")
+        col=get_user_input()
+        disc=desk[current_player]
+        grid=drop_disc(grid,col,disc)
+        if checkwin(grid,disc):
+            display_grid(grid)
+            print(f"the player{current_player+1} is winner")
+            break
+        else:
+            current_player=1-current_player  
+main()
