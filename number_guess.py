deb4b78c6aca41e5a0f72c17e20c2ef034cc99e1
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-11 23:58:53 +0530

    complete code in 3 minutes with all requirements

--- number_guess.py
+++ number_guess.py
@@ -7,3 +7,26 @@ def get_player_guess():
     guess=int(input("enter your number: "))
     return guess
 # print(get_player_guess())
+
+def feedback(num,guess):
+    
+    if num>guess:
+        return "too low"
+    elif num<guess:
+        return "too high"
+    else:
+        return "guessed"
+def checkwin(num,guess):
+    if num==guess:
+        return True
+    
+def main():
+    num=generateNumber()
+    while True:
+        guess=get_player_guess()
+        print(feedback(num,guess))
+        if checkwin(num,guess):
+            return "you guessed correct and won the game"
+main()
+    
+    
