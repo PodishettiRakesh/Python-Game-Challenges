dd8fb096c7125f5152d986dde010d06e515814b5
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-11 23:48:46 +0530

    get player guessed number from inputs

--- number_guess.py
+++ number_guess.py
@@ -3,4 +3,7 @@ def generateNumber():
     num=random.randint(1,100)
     return num
 # print(generateNumber())
-def get_
+def get_player_guess():
+    guess=int(input("enter your number: "))
+    return guess
+# print(get_player_guess())
