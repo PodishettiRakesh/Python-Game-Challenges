16a7f1d104db44726898304516bb9328d46ea88d
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-12 08:59:41 +0530

    fun to get secret word from list of words with using random.choice

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,14 @@
+import random
 def loadData():
     with open("Hangman_words.txt","r")as f:
         data=f.read()
     words=data.split("\n")
     words.pop()
-    print(words)
-loadData()
+    return words
+# loadData()
+
+def get_secret_word(words):
+    secret_word=random.choice(words)
+    print(secret_word)
+# words=loadData()
+# get_secret_word(words)
