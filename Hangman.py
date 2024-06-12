7008f12d041cd9c155d377d0837bed2b717af938
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-12 09:07:16 +0530

    display fun to show current state of word with guessed

--- Hangman.py
+++ Hangman.py
@@ -9,6 +9,17 @@ def loadData():
 
 def get_secret_word(words):
     secret_word=random.choice(words)
-    print(secret_word)
+    return secret_word
 # words=loadData()
 # get_secret_word(words)
+
+guessed=[]
+def display(word):
+    for i in word:
+        if i in guessed:
+            print(i,end=" ")
+        else:
+            print("_",end=" ")
+words=loadData()
+word=get_secret_word(words)
+display(word)
