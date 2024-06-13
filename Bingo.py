794b46c9f4111483c7ad84ec4b412f1be89e0504
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-06-13 11:10:26 +0530

    display function to dispaly the current state of the board

--- Bingo.py
+++ Bingo.py
@@ -1,11 +1,9 @@
 import random
 def generateBoard():
     numbers=random.sample(range(10,100),25)
-    # print(board)
     board=[[numbers.pop() for _ in range(5)]for _ in range(5)]
-    # print(board)
     return board
-# generateBoard()
+
 
 def display_board(board):
     for i in range(len(board)):
